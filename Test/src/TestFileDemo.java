b2fdf5475b5a5de9e552dfe50f801d8fa89ef69b
SaiMahesh07 <sarikondasaimahesh@gmail.com>
2022-12-15 22:39:58 -0500

    First commit

---
+++ Test/src/TestFileDemo.java
@@ -0,0 +1,12 @@
+
+
+public class TestFileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
---
+++ Test/src/com/demo/Demo1.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s="Mahesh";
+		int count=s.length();
+		System.out.println(count);
+	}
+
+}
